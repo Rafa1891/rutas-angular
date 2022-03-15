3b2d63aff6ee48a7b1ccc80776df123cbf551052
Rafa1891 <rufus199117@gmail.com>
2022-03-15 17:33:30 +0100

    rutas funciona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import { ComponentesModule } from './components/componentes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
-  imports: [
-    BrowserModule
+  imports:[
+    BrowserModule,
+    ComponentesModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
