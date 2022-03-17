f897e75ee252e8c89be3b06425c8dc73bcdc3000
Rafa1891 <rufus199117@gmail.com>
2022-03-17 08:36:29 +0100

    ej 4, 5

--- src/app/components/ejercicio4/ejercicio4.component.ts
+++ src/app/components/ejercicio4/ejercicio4.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/interfaces/empleado.interface';
 
 @Component({
   selector: 'app-ejercicio4',
@@ -6,6 +7,46 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class Ejercicio4Component implements OnInit {
+
+  @Input() empleados:Empleado[]=[];
+
+
+  @Input() nuevo:Empleado={
+    nombre:"",
+    sueldo:0
+  }
+
+resultado:string="";
+mayor:number=0;
+
+  mayorSueldo():void{
+    for(let i=0;i<this.empleados.length;i++){
+
+      if(this.empleados[i].sueldo>this.mayor){
+        this.mayor=this.empleados[i].sueldo;
+
+      }
+    }
+
+    this.resultado= "El mayor sueldo es: "+this.mayor;
+  }
+
+  mostrarNombres():void{
+    for(let i=0;i<this.empleados.length;i++){
+     this.resultado=this.empleados[i].nombre;
+    }
+
+  }
+
+  agregarEmpleado():void{
+    if(this.nuevo.nombre.trim().length===0){
+      return;
+    }
+  this.empleados.push(this.nuevo);
+  for(let i=0;i<this.empleados.length;i++){
+  console.log(this.empleados[i]);
+  }
+  }
 
   constructor() { }
 
--- src/app/components/ejercicio5/ejercicio5.component.ts
+++ src/app/components/ejercicio5/ejercicio5.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ejercicio5',
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ejercicio5Component implements OnInit {
 
+  @Input() presu:number=0;
+
+  urgencia:number=0.37;
+  pediatria:number=0.42;
+  traumatologia:number=0.21;
+
+  resultado:string="";
+
+  calcularPartidas(){
+
+    this.resultado="Para urgencias "+this.presu*this.urgencia+
+    ", para pediatria "+this.presu*this.pediatria+" y para traumatología "+this.presu*this.traumatologia+" euros.";
+  }
   constructor() { }
 
   ngOnInit(): void {
